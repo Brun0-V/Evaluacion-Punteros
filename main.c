0bc43614f2cba468b3b7964c3847cd9babd39ca0
Bruno <brunovega006@gmail.com>
2023-06-16 21:33:29 -0300

    Commented Main.c

--- main.c
+++ main.c
@@ -7,39 +7,56 @@ void convertirPrimerLetraMayuscula(char* oracion);
 
 
 int main() {
-    char stringInput[250];
-    printf("Ingrese una oracion de hasta 250 caracteres: "); scanf("%[^\n]s", stringInput);
-    printf("String original: %s", stringInput);
-    convertirTodoMayuscula(stringInput);
-    printf("String todo mayuscula: %s\n", stringInput);
-    convertirTodoMinuscula(stringInput);
-    printf("String todo minuscula %s\n", stringInput);
-    convertirPrimerLetraMayuscula(stringInput);
-    printf("String con la primera letra en mayuscula: %s\n", stringInput);
+    char stringInput[250];          //Inicializa un string de 250 caracteres.
+    printf("Ingrese una oracion de hasta 250 caracteres: "); scanf("%[^\n]s", stringInput);     //Pide al usuario que lo ingrese.
+    printf("String original: %s", stringInput);      //Imprime el string original
+    convertirTodoMayuscula(stringInput);        //Lo convierte a mayusculas
+    printf("String todo mayuscula: %s\n", stringInput);     //Imprime el string modificado
+    convertirTodoMinuscula(stringInput);        //Lo convierte a minusculas
+    printf("String todo minuscula %s\n", stringInput);     //Imprime el string modificado
+    convertirPrimerLetraMayuscula(stringInput);        //Cambia la primera letra de cada palabra por su mayuscula
+    printf("String con la primera letra en mayuscula: %s\n", stringInput);     //Imprime el string modificado
     return 0;
 }
 
+/**
+* Funcion que convierte un string de minuscula a mayuscula usando punteros
+* 
+* @param oracion - char Array
+*/
 void convertirTodoMayuscula(char* oracion){
-    do 
-    {
-        if (*oracion >= 'a' && *oracion <= 'z')
-            *oracion = *oracion - 32;
+    do {
+        if (*oracion >= 'a' && *oracion <= 'z')     
+            *oracion = *oracion - 32;                      
         oracion++;
-    } while (*oracion != '\0'); 
+    } while (*oracion != '\0');         
 }
 
+
+
+/**
+* Funcion que convierte un string de mayuscula a minuscula usando punteros
+* 
+* @param oracion - char Array
+*/
 void convertirTodoMinuscula(char* oracion){
-    do 
-    {
-        if (*oracion >= 'A' && *oracion <= 'Z')
-            *oracion = *oracion + 32;
+    do {
+        if (*oracion >= 'A' && *oracion <= 'Z')     //Revisa si el caracter al que se apunta es una letra mayuscula
+            *oracion = *oracion + 32;       //Sumando 32, en el codigo ASCII es esa misma letra en minuscula
         oracion++;
-    } while (*oracion != '\0'); 
+    } while (*oracion != '\0');          //Ejecuta el codigo hasta que el caracter '\0' (null) sea encontrado
 }
 
+
+
+/**
+* Funcion que convierte la primera letra de cada palabra en un string a mayuscula usando punteros
+* 
+* @param oracion - char Array
+*/
 void convertirPrimerLetraMayuscula(char* oracion){
     int first = 1;
-    do{   
+    do {   
         if (first){
             if (*oracion >= 'a' && *oracion <= 'z' && *oracion != ' '){
                 *oracion = *oracion - 32;
@@ -51,5 +68,9 @@ void convertirPrimerLetraMayuscula(char* oracion){
                 first = 1;
         }
         oracion++;
-    } while (*oracion != '\0'); 
+    } while (*oracion != '\0');          //Ejecuta el codigo hasta que el caracter '\0' (null) sea encontrado
 }
+
+/*
+
+*/
